8f0a752396bba7da69a1943d5b3c31aeffc009b4
이희근 <dasepa@ihuigeun-ui-MacBookPro.local>
2026-02-26 11:01:17 +0900

    Shrink font size of specific columns to 40%

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -44,15 +44,15 @@ export const Home: React.FC = () => {
                                     <th className="th-rank">현재 순위</th>
                                     <th className="th-name" style={{ textAlign: 'center' }}>순위 변동</th>
                                     <th className="th-name">종목명</th>
-                                    <th className="th-data">종가</th>
-                                    <th className="th-data">전일대비</th>
-                                    <th className="th-data">등락률 (%)</th>
-                                    <th className="th-data">NAV</th>
-                                    <th className="th-data">거래량 (만주)</th>
-                                    <th className="th-data">거래대금 (억원)</th>
-                                    <th className="th-data">시가총액 (억원)</th>
-                                    <th className="th-data">기준일</th>
-                                    <th className="th-data">모멘텀 점수</th>
+                                    <th className="th-data tiny-col">종가</th>
+                                    <th className="th-data tiny-col">전일대비</th>
+                                    <th className="th-data tiny-col">등락률 (%)</th>
+                                    <th className="th-data tiny-col">NAV</th>
+                                    <th className="th-data tiny-col">거래량 (만주)</th>
+                                    <th className="th-data tiny-col">거래대금 (억원)</th>
+                                    <th className="th-data tiny-col">시가총액 (억원)</th>
+                                    <th className="th-data tiny-col">기준일</th>
+                                    <th className="th-data tiny-col">모멘텀 점수</th>
                                     <th className="th-data">1주 수익률</th>
                                     <th className="th-data">2주 수익률</th>
                                     <th className="th-data">1개월 수익률</th>
@@ -80,17 +80,17 @@ export const Home: React.FC = () => {
                                             {item.name}
                                             {item.rank === 1 && <Trophy size={14} className="trophy-icon ml-1 inline-block" />}
                                         </td>
-                                        <td className="td-data" data-label="종가">{item.closePrice}</td>
-                                        <td className="td-data" data-label="전일대비">{item.diffPrev}</td>
-                                        <td className={`td-data text-${checkTrend(item.changeRate)}`} data-label="등락률 (%)">
+                                        <td className="td-data tiny-col" data-label="종가">{item.closePrice}</td>
+                                        <td className="td-data tiny-col" data-label="전일대비">{item.diffPrev}</td>
+                                        <td className={`td-data tiny-col text-${checkTrend(item.changeRate)}`} data-label="등락률 (%)">
                                             {item.changeRate}
                                         </td>
-                                        <td className="td-data" data-label="NAV">{item.nav}</td>
-                                        <td className="td-data" data-label="거래량 (만주)">{item.volume}</td>
-                                        <td className="td-data" data-label="거래대금 (억원)">{item.tradingValue}</td>
-                                        <td className="td-data" data-label="시가총액 (억원)">{item.marketCap}</td>
-                                        <td className="td-data" data-label="기준일">{item.baseDate}</td>
-                                        <td className="td-data font-medium text-purple-600" data-label="모멘텀 점수">
+                                        <td className="td-data tiny-col" data-label="NAV">{item.nav}</td>
+                                        <td className="td-data tiny-col" data-label="거래량 (만주)">{item.volume}</td>
+                                        <td className="td-data tiny-col" data-label="거래대금 (억원)">{item.tradingValue}</td>
+                                        <td className="td-data tiny-col" data-label="시가총액 (억원)">{item.marketCap}</td>
+                                        <td className="td-data tiny-col" data-label="기준일">{item.baseDate}</td>
+                                        <td className="td-data tiny-col font-medium text-purple-600" data-label="모멘텀 점수">
                                             {item.momentumScore}
                                         </td>
                                         {['yield1W', 'yield2W', 'yield1M', 'yield3M'].map((key) => {
